6d3096ff95014c71e333a82dfe6e8f3d2d8a3ad9
unknown <kshitij.dhawan310@gmail.com>
2022-10-18 19:44:56 +0530

    Bug fixes

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,157 +1,331 @@
-require('../models/database')
-const Category = require('../models/Category')
-const Recipe = require('../models/Recipe')
+require('../models/database');
+const Category = require('../models/Category');
+const Recipe = require('../models/Recipe');
 
-// homepage
+/**
+ * GET /
+ * Homepage 
+*/
+exports.homepage = async(req, res) => {
+  try {
+    const limitNumber = 5;
+    const categories = await Category.find({}).limit(limitNumber);
+    const latest = await Recipe.find({}).sort({_id: -1}).limit(limitNumber);
+    const thai = await Recipe.find({ 'category': 'Thai' }).limit(limitNumber);
+    const american = await Recipe.find({ 'category': 'American' }).limit(limitNumber);
+    const chinese = await Recipe.find({ 'category': 'Chinese' }).limit(limitNumber);
 
-exports.homepage = async (req, res) => {
-    try {
-        const limitNumber = 5
-        const categories = await Category.find({}).limit(limitNumber)
-        const latestRecipes = await Recipe.find({}).sort({_id: -1}).limit(limitNumber)
-        const thai = await Recipe.find({ 'category': 'Thai' }).limit(limitNumber);
-        const american = await Recipe.find({ 'category': 'American' }).limit(limitNumber);
-        const chinese = await Recipe.find({ 'category': 'Chinese' }).limit(limitNumber);
+    const food = { latest, thai, american, chinese };
 
-        const latest = { latestRecipes, thai, american, chinese }
-
-        res.render('index', { title: 'Cooking Blog- Homepage', categories, latest })
-    } catch (error) {
-        res.status(500).send({ message: error.message || "Error Occured" })
-    }
-
-}
-
-// for exploring the recipe categories page
-
-exports.exploreCategories = async (req, res) => {
-    try {
-        const limitNumber = 20
-        const categories = await Category.find({}).limit(limitNumber)
-        res.render('categories', { title: 'Cooking Blog- Categories', categories })
-    } catch (error) {
-        res.status(500).send({ message: error.message || "Error Occured" })
-    }
-
-}
-
-// for getting the recipe category by id
-
-exports.exploreCategoriesById = async(req, res) => { 
-    try {
-      let categoryId = req.params.id;
-      const limitNumber = 20;
-      const categoryById = await Recipe.find({ 'category': categoryId }).limit(limitNumber);
-      res.render('categories', { title: 'Cooking Blog - Categories', categoryById } );
-    } catch (error) {
-      res.satus(500).send({message: error.message || "Error Occured" });
-    }
+    res.render('index', { title: 'Cooking Blog - Home', categories, food } );
+  } catch (error) {
+    res.satus(500).send({message: error.message || "Error Occured" });
   }
-
-  //for gwtting the recipe of a particular dish
-
-exports.exploreRecipes = async (req,res) => {
-    try {
-        let recipeId = req.params.id
-        const recipe = await Recipe.findById(recipeId)
-        res.render('recipe', { title: 'Cooking Blog- Recipe', recipe })
-    } catch (error) {
-        res.status(500).send({ message: error.message || "Error Occured" })
-    }
 }
 
-// for searching recipes
+/**
+ * GET /categories
+ * Categories 
+*/
+exports.exploreCategories = async(req, res) => {
+  try {
+    const limitNumber = 20;
+    const categories = await Category.find({}).limit(limitNumber);
+    res.render('categories', { title: 'Cooking Blog - Categoreis', categories } );
+  } catch (error) {
+    res.satus(500).send({message: error.message || "Error Occured" });
+  }
+} 
 
+
+/**
+ * GET /categories/:id
+ * Categories By Id
+*/
+exports.exploreCategoriesById = async(req, res) => { 
+  try {
+    let categoryId = req.params.id;
+    const limitNumber = 20;
+    const categoryById = await Recipe.find({ 'category': categoryId }).limit(limitNumber);
+    res.render('categories', { title: 'Cooking Blog - Categoreis', categoryById } );
+  } catch (error) {
+    res.satus(500).send({message: error.message || "Error Occured" });
+  }
+} 
+ 
+/**
+ * GET /recipe/:id
+ * Recipe 
+*/
+exports.exploreRecipe = async(req, res) => {
+  try {
+    let recipeId = req.params.id;
+    const recipe = await Recipe.findById(recipeId);
+    res.render('recipe', { title: 'Cooking Blog - Recipe', recipe } );
+  } catch (error) {
+    res.satus(500).send({message: error.message || "Error Occured" });
+  }
+} 
+
+
+/**
+ * POST /search
+ * Search 
+*/
 exports.searchRecipe = async(req, res) => {
-    try {
-        let searchTerm = req.body.searchTerm;
+  try {
+    let searchTerm = req.body.searchTerm;
     let recipe = await Recipe.find( { $text: { $search: searchTerm, $diacriticSensitive: true } });
     res.render('search', { title: 'Cooking Blog - Search', recipe } );
-
-    } catch (error) {
-        res.status(500).send({ message: error.message || "Error Occured" })
-    }
-   
+  } catch (error) {
+    res.satus(500).send({message: error.message || "Error Occured" });
+  }
+  
 }
 
-// for exploring the latest recipes
-
+/**
+ * GET /explore-latest
+ * Explplore Latest 
+*/
 exports.exploreLatest = async(req, res) => {
-    try {
-      const limitNumber = 20;
-      const recipe = await Recipe.find({}).sort({ _id: -1 }).limit(limitNumber);
-      res.render('explore-latest', { title: 'Cooking Blog - Explore Latest', recipe } );
-    } catch (error) {
-      res.satus(500).send({message: error.message || "Error Occured" });
-    }
-  } 
-
-  // to explore a random recipe
-
-  exports.exploreRandom = async(req, res) => {
-    try {
-      let count = await Recipe.find().countDocuments();
-      let random = Math.floor(Math.random() * count);
-      let recipe = await Recipe.findOne().skip(random).exec();
-      res.render('explore-random', { title: 'Cooking Blog - Explore Latest', recipe } );
-    } catch (error) {
-      res.satus(500).send({message: error.message || "Error Occured" });
-    }
+  try {
+    const limitNumber = 20;
+    const recipe = await Recipe.find({}).sort({ _id: -1 }).limit(limitNumber);
+    res.render('explore-latest', { title: 'Cooking Blog - Explore Latest', recipe } );
+  } catch (error) {
+    res.satus(500).send({message: error.message || "Error Occured" });
   }
+} 
 
-  //GET
-  // To submit the recipe
 
-  exports.submitRecipe = async(req, res) => {
-    const infoErrorsObj = req.flash('infoErrors');
-    const infoSubmitObj = req.flash('infoSubmit');
-    res.render('submit-recipe', { title: 'Cooking Blog - Submit Recipe', infoErrorsObj, infoSubmitObj  } );
+
+/**
+ * GET /explore-random
+ * Explore Random as JSON
+*/
+exports.exploreRandom = async(req, res) => {
+  try {
+    let count = await Recipe.find().countDocuments();
+    let random = Math.floor(Math.random() * count);
+    let recipe = await Recipe.findOne().skip(random).exec();
+    res.render('explore-random', { title: 'Cooking Blog - Explore Latest', recipe } );
+  } catch (error) {
+    res.satus(500).send({message: error.message || "Error Occured" });
   }
+} 
 
-   //POST
-  // To submit the recipe
 
-  exports.submitRecipeOnPost = async(req, res) => {
-    try {
+/**
+ * GET /submit-recipe
+ * Submit Recipe
+*/
+exports.submitRecipe = async(req, res) => {
+  const infoErrorsObj = req.flash('infoErrors');
+  const infoSubmitObj = req.flash('infoSubmit');
+  res.render('submit-recipe', { title: 'Cooking Blog - Submit Recipe', infoErrorsObj, infoSubmitObj  } );
+}
+
+/**
+ * POST /submit-recipe
+ * Submit Recipe
+*/
+exports.submitRecipeOnPost = async(req, res) => {
+  try {
+
+    let imageUploadFile;
+    let uploadPath;
+    let newImageName;
+
+    if(!req.files || Object.keys(req.files).length === 0){
+      console.log('No Files where uploaded.');
+    } else {
+
+      imageUploadFile = req.files.image;
+      newImageName = Date.now() + imageUploadFile.name;
+
+      uploadPath = require('path').resolve('./') + '/public/uploads/' + newImageName;
+
+      imageUploadFile.mv(uploadPath, function(err){
+        if(err) return res.satus(500).send(err);
+      })
+
+    }
+
+    const newRecipe = new Recipe({
+      name: req.body.name,
+      description: req.body.description,
+      email: req.body.email,
+      ingredients: req.body.ingredients,
+      category: req.body.category,
+      image: newImageName
+    });
+    
+    await newRecipe.save();
+
+    req.flash('infoSubmit', 'Recipe has been added.')
+    res.redirect('/submit-recipe');
+  } catch (error) {
+    // res.json(error);
+    req.flash('infoErrors', error);
+    res.redirect('/submit-recipe');
+  }
+}
+
+
+// require('../models/database')
+// const Category = require('../models/Category')
+// const Recipe = require('../models/Recipe')
+
+// // homepage
+
+// exports.homepage = async (req, res) => {
+//     try {
+//         const limitNumber = 5
+//         const categories = await Category.find({}).limit(limitNumber)
+//         const latestRecipes = await Recipe.find({}).sort({_id: -1}).limit(limitNumber)
+//         const thai = await Recipe.find({ 'category': 'Thai' }).limit(limitNumber);
+//         const american = await Recipe.find({ 'category': 'American' }).limit(limitNumber);
+//         const chinese = await Recipe.find({ 'category': 'Chinese' }).limit(limitNumber);
+
+//         const latest = { latestRecipes, thai, american, chinese }
+
+//         res.render('index', { title: 'Cooking Blog- Homepage', categories, latest })
+//     } catch (error) {
+//         res.status(500).send({ message: error.message || "Error Occured" })
+//     }
+
+// }
+
+// // for exploring the recipe categories page
+
+// exports.exploreCategories = async (req, res) => {
+//     try {
+//         const limitNumber = 20
+//         const categories = await Category.find({}).limit(limitNumber)
+//         res.render('categories', { title: 'Cooking Blog- Categories', categories })
+//     } catch (error) {
+//         res.status(500).send({ message: error.message || "Error Occured" })
+//     }
+
+// }
+
+// // for getting the recipe category by id
+
+// exports.exploreCategoriesById = async(req, res) => { 
+//     try {
+//       let categoryId = req.params.id;
+//       const limitNumber = 20;
+//       const categoryById = await Recipe.find({ 'category': categoryId }).limit(limitNumber);
+//       res.render('categories', { title: 'Cooking Blog - Categories', categoryById } );
+//     } catch (error) {
+//       res.satus(500).send({message: error.message || "Error Occured" });
+//     }
+//   }
+
+//   //for gwtting the recipe of a particular dish
+
+// exports.exploreRecipes = async (req,res) => {
+//     try {
+//         let recipeId = req.params.id
+//         const recipe = await Recipe.findById(recipeId)
+//         res.render('recipe', { title: 'Cooking Blog- Recipe', recipe })
+//     } catch (error) {
+//         res.status(500).send({ message: error.message || "Error Occured" })
+//     }
+// }
+
+// // for searching recipes
+
+// exports.searchRecipe = async(req, res) => {
+//     try {
+//         let searchTerm = req.body.searchTerm;
+//     let recipe = await Recipe.find( { $text: { $search: searchTerm, $diacriticSensitive: true } });
+//     res.render('search', { title: 'Cooking Blog - Search', recipe } );
+
+//     } catch (error) {
+//         res.status(500).send({ message: error.message || "Error Occured" })
+//     }
+   
+// }
+
+// // for exploring the latest recipes
+
+// exports.exploreLatest = async(req, res) => {
+//     try {
+//       const limitNumber = 20;
+//       const recipe = await Recipe.find({}).sort({ _id: -1 }).limit(limitNumber);
+//       res.render('explore-latest', { title: 'Cooking Blog - Explore Latest', recipe } );
+//     } catch (error) {
+//       res.satus(500).send({message: error.message || "Error Occured" });
+//     }
+//   } 
+
+//   // to explore a random recipe
+
+//   exports.exploreRandom = async(req, res) => {
+//     try {
+//       let count = await Recipe.find().countDocuments();
+//       let random = Math.floor(Math.random() * count);
+//       let recipe = await Recipe.findOne().skip(random).exec();
+//       res.render('explore-random', { title: 'Cooking Blog - Explore Latest', recipe } );
+//     } catch (error) {
+//       res.satus(500).send({message: error.message || "Error Occured" });
+//     }
+//   }
+
+//   //GET
+//   // To submit the recipe
+
+//   exports.submitRecipe = async(req, res) => {
+//     const infoErrorsObj = req.flash('infoErrors');
+//     const infoSubmitObj = req.flash('infoSubmit');
+//     res.render('submit-recipe', { title: 'Cooking Blog - Submit Recipe', infoErrorsObj, infoSubmitObj  } );
+//   }
+
+//    //POST
+//   // To submit the recipe
+
+//   exports.submitRecipeOnPost = async(req, res) => {
+//     try {
   
-      let imageUploadFile;
-      let uploadPath;
-      let newImageName;
+//       let imageUploadFile;
+//       let uploadPath;
+//       let newImageName;
   
-      if(!req.files || Object.keys(req.files).length === 0){
-        console.log('No Files where uploaded.');
-      } else {
+//       if(!req.files || Object.keys(req.files).length === 0){
+//         console.log('No Files where uploaded.');
+//       } else {
   
-        imageUploadFile = req.files.image;
-        newImageName = Date.now() + imageUploadFile.name;
+//         imageUploadFile = req.files.image;
+//         newImageName = Date.now() + imageUploadFile.name;
   
-        uploadPath = require('path').resolve('./') + '/public/uploads/' + newImageName;
+//         uploadPath = require('path').resolve('./') + '/public/uploads/' + newImageName;
   
-        imageUploadFile.mv(uploadPath, function(err){
-          if(err) return res.satus(500).send(err);
-        })
+//         imageUploadFile.mv(uploadPath, function(err){
+//           if(err) return res.satus(500).send(err);
+//         })
   
-      }
+//       }
   
-      const newRecipe = new Recipe({
-        name: req.body.name,
-        description: req.body.description,
-        email: req.body.email,
-        ingredients: req.body.ingredients,
-        category: req.body.category,
-        image: newImageName
-      });
+//       const newRecipe = new Recipe({
+//         name: req.body.name,
+//         description: req.body.description,
+//         email: req.body.email,
+//         ingredients: req.body.ingredients,
+//         category: req.body.category,
+//         image: newImageName
+//       });
       
-      await newRecipe.save();
+//       await newRecipe.save();
   
-      req.flash('infoSubmit', 'Recipe has been added.')
-      res.redirect('/submit-recipe');
-    } catch (error) {
-      // res.json(error);
-      req.flash('infoErrors', error);
-      res.redirect('/submit-recipe');
-    }
-  }
+//       req.flash('infoSubmit', 'Recipe has been added.')
+//       res.redirect('/submit-recipe');
+//     } catch (error) {
+//       // res.json(error);
+//       req.flash('infoErrors', error);
+//       res.redirect('/submit-recipe');
+//     }
+//   }
 
 // function for inserting dummy data
 
